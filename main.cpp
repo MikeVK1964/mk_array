8046f727a184cd190963d93538b42e6c513dbb8a
MikeVK1964 <163735299+MikeVK1964@users.noreply.github.com>
2024-07-17 22:01:08 +0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <cassert>
+
+using namespace std;
+template <class T,size_t mcount>
+class mk_array {
+ friend ostream& operator <<(ostream& output, mk_array ar)
+ {
+    for (auto i=0;i<ar.count;i++)
+        output << ar[i];
+    return output;  // для cout << a << b;
+ }
+public:
+    mk_array() {
+        if (mcount==0) throw "aaa";
+        parr = new T[mcount];
+         assert(parr!=nullptr);
+         count = mcount;
+    }
+    T& operator [](int i) {
+        if (i<0 || count <=static_cast<size_t>(i) ) throw "aa";
+        return parr[i];
+    }
+
+    ~mk_array() { delete [] parr;}
+private:
+    T* parr;
+    size_t count;
+};
+
+int main()
+{
+    cout << "Hello World!" << endl;
+    mk_array<int,6> ar;
+    ar[0]=1;
+    std::cout << ar[0] << endl;
+  //  cout << ar;  сгенерирует operator<<(cout,arr);
+
+    return 0;
+}
